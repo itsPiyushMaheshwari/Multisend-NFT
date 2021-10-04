9e7952692ac8a4e465da800fbcb8cb94f7fcc3c7
Piyush Maheshwari <70137264+itsPiyushMaheshwari@users.noreply.github.com>
2021-10-04 10:03:46 +0530

    .

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,35 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
 require('hardhat-abi-exporter');
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config()
 
-const ETH_NODE_URL = process.env.ETH_NODE_URL;
-
-/*
-// example task
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-*/
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+// const fs = require('fs');
+// const privateKey = fs.readFileSync(".secret").toString().trim();
 module.exports = {
-  //defaultNetwork: "rinkeby",
+  defaultNetwork: "polygon",
   networks: {
     hardhat: {
-      chainId: 1337,
-      forking: {
-        url: ETH_NODE_URL
-      },
-      rinkeby: {
-        url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-        accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"]
-      }
     },
+      polygon: {
+        url: "https://rpc-mumbai.maticvigil.com/v1/bbcd12537cd9f228a8a54b0bc8b4f4db124000d5",
+        accounts: [process.env.PRIVATE_KEY],
+        // HttpNetworkConfig:80001
+      }
   },
   solidity: {
     version: "0.8.4",
@@ -40,19 +25,5 @@ module.exports = {
       }
     }
   },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 20000
-  },
-  abiExporter: {
-    path: './abi/',
-    clear: true,
-    flat: true,
-    spacing: 2
-  }
+
 };
